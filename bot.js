637a029444b4695cc8d8b5f8c12fc1acfd109589
Vincent Mahnke <v.mahnke@bigpoint.net>
2018-01-30 14:52:59 +0100

    ~ Moved string literals to environment variables + Support for re-runs + Ability to leave out messages

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+// Helper method
+const expand = require('expandJS');
+String.prototype.format = expand.stringFormat;
+
 // Main discord functionality
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -6,7 +10,16 @@ const Date = require('datejs');
 const Request = require('async-request');
 
 // globals
-let TWITCH_CHANNEL_NAME = ""
+let TWITCH_CHANNEL_NAME = "";
+
+const LiveTypes = ["RERUN", "OFFLINE", "LIVE"];
+
+
+function GetStateSpecificEnvironmentVariable(state, variableName)
+{
+    return process.env[`IS${state}_${variableName}`].replace("\\n", "\n") || "";
+}
+
 
 async function GetPopularClipLastMonth()
 {
@@ -26,7 +39,7 @@ async function GetPopularClipLastMonth()
     }
 
     const parsedResponse = JSON.parse(response.body);
-    
+
     return parsedResponse.clips[0];
 }
 
@@ -48,7 +61,7 @@ async function GetUserData(userID)
     }
 
     const parsedResponse = JSON.parse(response.body);
-    
+
     return parsedResponse;
 }
 
@@ -70,25 +83,42 @@ async function GetGameData(gameID)
     }
 
     const parsedResponse = JSON.parse(response.body);
-    
+
     return parsedResponse.data[0];
 }
 
-async function PrintLiveEmbed(streamData)
+async function PrintLiveEmbed(state, streamData)
 {
     const userData = await GetUserData(streamData.user_id);
     const gameData = await GetGameData(streamData.game_id);
 
+    if (!userData)
+    {
+        console.warn(`We're unable to find user ${streamData.user_id}; not printing any embed message!`);
+    }
+
+    if (!gameData)
+    {
+        console.warn(`We're unable to find game ${streamData.game_id}; not printing any embed message!`);
+    }
+
+    if (!userData || !gameData)
+    {
+        return;
+    }
+
     // filter people streaming
     let matches = [];
-    const regex = /(?:\| | und )(Vincent|Chris|Hanna|Viki|Minh)/g;
+    let people = process.env.PEOPLE.split(',');
+    console.log(`[INTERNAL] Trying to find '${people.join(',')}' in the stream title...`)
+    const regex = new RegExp('(?:\| | und )('+people.join('|')+')', 'g');
     let m;
 
     while ((m = regex.exec(streamData.title)) !== null) {
         if (m.index === regex.lastIndex) {
             regex.lastIndex++;
         }
-        
+
         m.forEach((match, groupIndex) => {
             if (groupIndex == 1)
             {
@@ -98,17 +128,17 @@ async function PrintLiveEmbed(streamData)
     }
 
     let embed = new Discord.RichEmbed()
-        .setAuthor("WIR SIND LIVE", userData.logo, `https://twitch.tv/${TWITCH_CHANNEL_NAME}`)
-        .setColor(0x6441A5)
-        .setImage(streamData.thumbnail_url.replace("{width}", 1280).replace("{height}", 720))
-        .setThumbnail(gameData.box_art_url.replace("{width}", 136).replace("{height}", 190))
-        .setURL(`https://twitch.tv/${TWITCH_CHANNEL_NAME}`)
-        .addField("On-Air mit...", gameData.name, true)
+    .setAuthor(GetStateSpecificEnvironmentVariable(state, "EMBED_TITLE"), userData.logo, `https://twitch.tv/${TWITCH_CHANNEL_NAME}`)
+    .setColor(0x6441A5)
+    .setImage(streamData.thumbnail_url.replace("{width}", 1280).replace("{height}", 720))
+    .setThumbnail(gameData.box_art_url.replace("{width}", 136).replace("{height}", 190))
+    .setURL(`https://twitch.tv/${TWITCH_CHANNEL_NAME}`)
+    .addField(GetStateSpecificEnvironmentVariable(state, "EMBED_GAME_PREFIX"), gameData.name, true)
 
     if (matches.length != 0)
     {
         embed.setTitle(streamData.title.replace(/(\ \| ([^\|]*))$/g, ""));
-        embed.addField(`Am Start ${matches.length==1?'ist':'sind'}...`, matches.join(" & ") + "!", true);
+        embed.addField(GetStateSpecificEnvironmentVariable(state, "EMBED_PEOPLE_PREFIX"), matches.join(" & ") + "!", true);
     }
     else
     {
@@ -116,6 +146,49 @@ async function PrintLiveEmbed(streamData)
     }
 
     return embed;
+}
+
+async function SendMessages(state, data)
+{
+    const messageFormat = GetStateSpecificEnvironmentVariable(state, "MESSAGE");
+    if (messageFormat.length > 0)
+    {
+        const clip = await GetPopularClipLastMonth();
+        if (typeof clip != "undefined")
+        {
+            const message = messageFormat.format({clipURL: clip.url, twitchChannelName: TWITCH_CHANNEL_NAME})
+            console.log(`[DISCORD] Sending regular message:`);
+            console.log(`[DISCORD] ${message}`);
+            client.channels.find("id", process.env.DISCORD_CHANNEL_ID).send(message);
+        }
+        else
+        {
+            console.log(`[DISCORD] Requested to send a message about ${state}, but there is no clip in the last month... Skipping the regular text!`);
+        }
+    }
+    else
+    {
+        console.log(`[DISCORD] Not sending a message, as there's no MESSAGE for '${state}' set`);
+    }
+
+    if (GetStateSpecificEnvironmentVariable(state, "EMBED_TITLE"))
+    {
+        const embedFormat = await PrintLiveEmbed(state, data);
+        if (embedFormat)
+        {
+            console.log(`Sending embed message:`);
+            console.log(`[DISCORD] ${JSON.stringify(embedFormat)}`);
+            client.channels.find("id", process.env.DISCORD_CHANNEL_ID).send(embedFormat);
+        }
+        else
+        {
+            console.log(`[DISCORD] Requested to send an embed message about ${state}, but we don't have sufficient information; check the log!`);
+        }
+    }
+    else
+    {
+        console.log(`[DISCORD] Not sending a message, as there's no EMBED_TITLE for '${state}' set`);
+    }
 }
 
 function CheckOnlineStatus(client) {
@@ -129,32 +202,10 @@ function CheckOnlineStatus(client) {
 
     async function OnOnline(data)
     {
-        console.log(`[TWITCH] Stream came online! Preparing message...`);
-        
-        const clip = await GetPopularClipLastMonth();
-        if (typeof clip != "undefined")
-        {
-            const messageFormat = `Hey @everyone! ${TWITCH_CHANNEL_NAME} ist live! Jetzt einschalten oder sowas hier verpassen: ${clip.url}`;
-            console.log(`[TWITCH] Sending regular message:`);
-            console.log(`[TWITCH] ${messageFormat}`);
-            client.channels.find("id", process.env.DISCORD_CHANNEL_ID).send(messageFormat);
-        }
-        else
-        {
-            console.log(`[TWITCH] Stream is online, but there is no clip in the last month... Skipping the regular text!`);
-        }
-
-        const embedFormat = await PrintLiveEmbed(data);
-        console.log(`Sending embed message:`);
-        console.log(`[TWITCH] ${JSON.stringify(embedFormat)}`);
-        client.channels.find("id", process.env.DISCORD_CHANNEL_ID).send(embedFormat);
-
-        console.log(`Sending done!`);
     }
 
     function OnOffline(data)
     {
-        console.log(`Stream went offline!`);
     }
 
     function CollectState(state)
@@ -164,8 +215,8 @@ function CheckOnlineStatus(client) {
             upcomingState = state;
             currentValidationSteps = 1;
         }
-    
-        console.log(`[INTERN] Collecting state ${state ? "online" : "offline"}: Stage ${currentValidationSteps} / ${requiredValidationSteps}`);
+
+        console.log(`[INTERNAL] Collecting state ${state ? "online" : "offline"}: Stage ${currentValidationSteps} / ${requiredValidationSteps}`);
 
         if (currentValidationSteps < requiredValidationSteps)
         {
@@ -195,22 +246,36 @@ function CheckOnlineStatus(client) {
             return;
         }
 
-        if (typeof currentState == "undefined")
-        {
-            currentState = newState;
-            console.log(`[TWITCH] Not executing logic for initial state setup; initial state: "${currentState?"online":"offline"}"!`);
-            return;
-        }
-
         currentState = newState;
-        if (newState)
+        OnStateChange(newState, data);
+    }
+
+    const stateCallbacks = {
+        "OFFLINE": async function (data)
         {
-            OnOnline(data);
-        }
-        else
+            console.log(`Stream went offline!`);
+        },
+        "RERUN": async function (data)
         {
-            OnOffline();
+            console.log(`[TWITCH] Stream is now playing reruns! Preparing messages...`);
+
+            SendMessages("RERUN", data);
+
+            console.log(`[TWITCH] Sending done!`);
+        },
+        "LIVE": async function (data)
+        {
+            console.log(`[TWITCH] Stream is live! Preparing messages...`);
+
+            SendMessages("LIVE", data);
+
+            console.log(`[TWITCH] Sending done!`);
         }
+    };
+
+    async function OnStateChange(state, data)
+    {
+        stateCallbacks[state](data);
     }
 
     async function OnUpdate()
@@ -230,8 +295,21 @@ function CheckOnlineStatus(client) {
         }
 
         const parsedResponse = JSON.parse(response.body);
-        const streamIsOnline = parsedResponse.data.length > 0;
-        UpdateState(streamIsOnline, streamIsOnline ? parsedResponse.data[0] : {});
+        let data = {
+            user_id: 0,
+            game_id: 0,
+            title: "NO TITLE",
+            thumbnail_url: ""
+        };
+        let streamState = LiveTypes.OFFLINE;
+
+        if (parsedResponse.data.length > 0)
+        {
+            data = parsedResponse.data[0];
+            streamState = data.type.toUpperCase();
+        }
+
+        UpdateState(streamState, data);
         return;
     }
 
@@ -239,14 +317,14 @@ function CheckOnlineStatus(client) {
 }
 
 client.on('ready', async () => {
-	console.log(`[DISCORD] Discord library is ready!`);
-	console.log(`[DISCORD] Logged in as ${client.user.tag}!`);
+    console.log(`[DISCORD] Discord library is ready!`);
+    console.log(`[DISCORD] Logged in as ${client.user.tag}!`);
 
-	console.log(`[TWITCH] Resolving twitch-ID ${process.env.TWITCH_CHANNEL_ID}...`);
-	TWITCH_CHANNEL_NAME = await GetUserData(process.env.TWITCH_CHANNEL_ID);
-	TWITCH_CHANNEL_NAME = TWITCH_CHANNEL_NAME.name;
-	console.log(`[TWITCH] Resolved ${process.env.TWITCH_CHANNEL_ID} to /${TWITCH_CHANNEL_NAME}!`);
-	console.log(`[TWITCH] Waiting for twitch channel ${TWITCH_CHANNEL_NAME} to come online!`);
+    console.log(`[TWITCH] Resolving twitch-ID ${process.env.TWITCH_CHANNEL_ID}...`);
+    TWITCH_CHANNEL_NAME = await GetUserData(process.env.TWITCH_CHANNEL_ID);
+    TWITCH_CHANNEL_NAME = TWITCH_CHANNEL_NAME.name;
+    console.log(`[TWITCH] Resolved ${process.env.TWITCH_CHANNEL_ID} to /${TWITCH_CHANNEL_NAME}!`);
+    console.log(`[TWITCH] Waiting for twitch channel ${TWITCH_CHANNEL_NAME} to come online!`);
 
     let statusChecker = CheckOnlineStatus(client);
     statusChecker();
@@ -262,7 +340,13 @@ function tablefy(content)
         "TWITCH_CLIENT_ID": false,
         "TWITCH_CHANNEL_ID": true,
         "DISCORD_CHANNEL_ID" : true,
-        "DISCORD_USER_TOKEN": false
+        "DISCORD_USER_TOKEN": false,
+        "VALIDATION_STEPS_REQUIRED": true,
+        "PEOPLE": true,
+        "EMBED_TITLE": true,
+        "EMBED_GAME_PREFIX": true,
+        "EMBED_PEOPLE_PREFIX": true,
+        "EMBED_POST": true
     };
 
     let output = "";
@@ -273,15 +357,7 @@ function tablefy(content)
         {
             continue;
         }
-
-        output += "<tr>";
-        output += "<th>";
-        output += key;
-        output += "</th>";
-        output += "<td>";
-        output += keyWhitelist[key] ? content[key] : "<i>REDACTED</i>";
-        output += "</td>";
-        output += "</tr>";
+        output += `<tr><th>${key}</th><td>${keyWhitelist[key] ? content[key] : "<i>REDACTED</i>"}</td></tr>`
     }
     output += "</table>";
 
@@ -302,7 +378,7 @@ require('http').createServer((request, response) => {
         body = Buffer.concat(body).toString();
 
         response.on('error', (err) => {
-          console.error(err);
+            console.error(err);
         });
 
         response.statusCode = 200;
